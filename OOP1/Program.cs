5c9bdd4ab95a0512f5169227f2366363e33ecc4c
israfilhallac <i.hallac1408@gmail.com>
2021-02-22 06:27:05 +0200

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product {
+                Id = 1, CategoryId = 2, ProductName = "Olta Takımı",
+                UnitPrice = 450, UnitsInStock = 3
+            };
+
+            Product product2 = new Product {
+                Id = 2, CategoryId = 3, 
+                ProductName = "Misina", UnitPrice = 100, UnitsInStock = 4 
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
